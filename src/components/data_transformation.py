68247e00d8d88cdae4c76ef9fffdc79688164200
waqas <waqasliaqat630@gmail.com>
2025-01-30 14:35:09 +0500

    model_training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
-from sklearn.metrics import r2_score
+# Metrics and tuning
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
@@ -42,9 +43,9 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            train_model_score = accuracy_score(y_train, y_train_pred)
 
-            test_model_score = r2_score(y_test, y_test_pred)
+            test_model_score = accuracy_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
